c98974c095259803135f148877d19ab8890ff1cb
karolinakv <karolinavalentova2@gmail.com>
2019-10-16 17:26:33 +0200

    added fetch from json and weather forcast

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 "use strict";
 
+let todayIs = '';
 
 function doStart() {
+    doFormatDateAndTime();
+    doRefreshData();
+    doGetCurrentWeather()
+    setInterval(() => {
+        if(todayIs !== new Date().toLocaleDateString('en-GB', { weekday: 'long'})) doRefreshData();
+        doFormatDateAndTime();
+    }, 1500);
+}
+
+function doFormatDateAndTime(){
     formatDate(document.getElementById('currentDate'));
     formateTime(document.getElementById('currentTime'));
+}
+
+function doRefreshData() {
+    deleteChilds(document.getElementById('tasksTable'));
+    processTasks().then();
 }
 
 function formatDate(element) {
@@ -14,6 +30,8 @@ function formatDate(element) {
         monthName: new Date().toLocaleDateString('en-GB', { month: 'long'}),
         year: new Date().toLocaleDateString('en-GB', { year: 'numeric'}),
     };
+
+    todayIs = dateObject.dayName;
 
     function doGetDayOrdinal(day) {
         const ordinalValue = day % 10;
@@ -35,6 +53,35 @@ function formateTime(element) {
 }
 
 
+function doGetCurrentWeather() {
+    const copenhagenCityString = 'Copenhagen,dk';
+    const key = 'af3c97ae17d94dfc69fe90e9b0eaacca';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + copenhagenCityString+ '&units=metric&APPID=' + key)
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+            console.table(data);
+            const main = data.main;
+            const weather = data.weather;
+
+
+            document.getElementById('temperature').textContent = parseInt(main['temp']) + '°C';
+            document.getElementById('humidity').textContent = main['humidity'] + '%';
+
+            const conditionsElement = document.getElementById('conditions');
+            if(Array.isArray(weather)) {
+                weather.forEach((condition) => {
+                    if(conditionsElement.innerText === '') conditionsElement.innerText += condition['main'] ; // Will look like ' Fog'
+                    else conditionsElement.innerText += condition['main'] ; // Will look like ' Fog , cold, rainy'
+                })
+            } else {
+                conditionsElement.innerText = weather['main'];
+            }
+        })
+        .catch(function() {
+            // catch any errors
+        });
+}
+
 function doChangeTitle(event) {
     event.preventDefault();
 
@@ -50,4 +97,50 @@ function doChangeStyle(newClass) {
     previousStyleClass = newClass;
 
     bodyElement.classList.add(newClass);
+}
+
+let tasksData;
+
+async function processTasks() {
+    try {
+        // Calling fetch within a function in order to pass the returned data to .json() to be converted into a javascript object
+        const jsonTasks = await (await fetch("./tasks.json")).json();
+
+        tasksData = jsonTasks ? jsonTasks : [];
+        showTasksData(tasksData);
+
+    } catch(error) {
+        tasksData = [];
+        console.error('Cannot read tasks list, reason: ' + error.message);
+    }
+}
+
+function showTasksData() {
+    const taskElement = document.getElementById('tasksTable');
+    const taskEntryTemplate = document.getElementById('taskEntry');
+
+    tasksData.forEach((taskEntry) => {
+
+        let temporaryTaskTemplate = taskEntryTemplate.content.cloneNode(true);
+        const newTaskEntry = buildTaskEntry(temporaryTaskTemplate, taskEntry);
+
+        if(newTaskEntry) taskElement.appendChild(newTaskEntry);
+    });
+}
+
+function buildTaskEntry(element, taskData) {
+    if(taskData.day === new Date().toLocaleDateString('en-GB', { weekday: 'long'})){
+        element.firstElementChild.children[0].textContent = taskData.time;
+        element.firstElementChild.children[2].textContent = taskData.description;
+
+        return element;
+    }
+}
+
+function deleteChilds(parentElement) {
+    let child = parentElement.lastElementChild;
+    while(child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
 }
